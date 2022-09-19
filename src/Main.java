ce0c3ba32a77c9bd7cc7e1a9e4b00cd60c2d4fff
Igor <99yearago99@gmail.com>
2022-09-19 13:54:29 +0300

    Second task

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,5 @@ public class Main {
         System.out.println(friends);
         friends = friends / 7;
         System.out.println(friends);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog *10;
-        System.out.println(frog);
-        frog = frog / 3;
-        System.out.println(frog);
-        frog = frog *4;
-        System.out.println(frog);
     }
 }
