1c898ea3861c7e4b96d716ee16a4df9aa5d5b319
Igor <99yearago99@gmail.com>
2022-09-19 13:47:25 +0300

    First task

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var friends = 19;
+        System.out.println(friends);
+        friends = friends + 2;
+        System.out.println(friends);
+        friends = friends / 7;
+        System.out.println(friends);
+    }
+}
